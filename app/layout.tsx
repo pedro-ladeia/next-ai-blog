ca6a73f07839637945f17031190d8860ef9eee37
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-05-21 21:57:36 -0300

    feat: Added navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from './(shared)/Navbar'
 import './globals.css'
 import { Open_Sans } from 'next/font/google'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html className={open_sans.className} lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
