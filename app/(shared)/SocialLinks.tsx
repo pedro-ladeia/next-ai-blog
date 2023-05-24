a93076168d0e82d8c3066c2acafd7902dcbcc31a
Pedro H M Ladeia <pedro.ladeia02@gmail.com>
2023-05-24 12:19:25 -0300

    feat: Added the imgs on component and hover effect

--- app/(shared)/SocialLinks.tsx
+++ app/(shared)/SocialLinks.tsx
@@ -1,11 +1,70 @@
 import React from 'react'
 import Image from 'next/image'
+import Twitter from 'public/assets/social_twitter.png'
+import Instagram from 'public/assets/social_instagram.png'
+import Facebook from 'public/assets/social_facebook.png'
+import Google from 'public/assets/social_google.png'
+import Discord from 'public/assets/social_discord.png'
 
-type Props = {}
 
-const SocialLinks = (props: Props) => {
+type Props = {
+  isDark?: boolean;
+}
+
+const SocialLinks = ({ isDark = false}: Props) => {
   return (
-    <div>SocialLinks</div>
+    <div className='flex justify-between items-center gap-7'>
+      <a href="https://twitter.con" target="_blank" rel="noreferrer">
+        <Image
+        className={`${isDark ? 'brightness-0' : ''} hover:opacity-50`}
+          src={Twitter} 
+          alt='Twitter logo'
+          width={20}
+          height={20}
+          />
+      </a>
+
+      <a href="https://instagram.con" target="_blank" rel="noreferrer">
+        <Image
+        className={`${isDark ? 'brightness-0' : ''} hover:opacity-50`}
+          src={Instagram} 
+          alt='Instagram logo'
+          width={20}
+          height={20}
+          />
+      </a>
+
+      <a href="https://facebook.con" target="_blank" rel="noreferrer">
+        <Image
+        className={`${isDark ? 'brightness-0' : ''} hover:opacity-50`}
+          src={Facebook} 
+          alt='Facebook logo'
+          width={20}
+          height={20}
+          />
+      </a>
+
+      <a href="https://google.con" target="_blank" rel="noreferrer">
+        <Image
+        className={`${isDark ? 'brightness-0' : ''} hover:opacity-50`}
+          src={Google} 
+          alt='Google logo'
+          width={20}
+          height={20}
+          />
+      </a>
+
+      <a href="https://discord.con" target="_blank" rel="noreferrer">
+        <Image
+        className={`${isDark ? 'brightness-0' : ''} hover:opacity-50`}
+          src={Discord} 
+          alt='discord logo'
+          width={20}
+          height={20}
+          />
+      </a>
+
+    </div>
   )
 }
 
