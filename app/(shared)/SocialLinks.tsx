28c5856fb984912dc1d2a89411cd535a0d9e1e50
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-05-23 22:35:18 -0300

    feat: Created the social links component, must add the icon img

---
+++ app/(shared)/SocialLinks.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+type Props = {}
+
+const SocialLinks = (props: Props) => {
+  return (
+    <div>SocialLinks</div>
+  )
+}
+
+export default SocialLinks
