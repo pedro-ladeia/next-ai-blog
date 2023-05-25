98420823f08a58bc485f44f720e62a4dfdacd689
Pedro H M Ladeia <pedro.ladeia02@gmail.com>
2023-05-25 17:40:22 -0300

    feat: Added the first column

---
+++ app/(shared)/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+    return (
+        <footer className='bg-wh-900 text-wh-50 px-10'>
+            <div className='justify-between mx-auto gap-16 sm:flex '>
+                {/*First Column */}
+
+                <div className='mt-16 basis-1/2 sm:mt-0'>
+                    <h4 className='font-bold'>THE AI BLOG</h4>
+                    <p className='my-5'>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500
+                    </p>
+                    <p>The AI blog All Rights Reserved</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
