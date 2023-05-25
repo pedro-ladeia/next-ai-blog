74701c99217d81f64210940bdb31ee07c2f4e34a
Pedro H M Ladeia <pedro.ladeia02@gmail.com>
2023-05-25 17:40:01 -0300

    feat: Addde Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from './(shared)/Footer'
 import Navbar from './(shared)/Navbar'
 import './globals.css'
 import { Open_Sans } from 'next/font/google'
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Footer />
         </body>
     </html>
   )
