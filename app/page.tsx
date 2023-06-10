967a16bc555d30950b5eb07f9cd5779222a3395a
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-06-10 13:33:44 -0300

    :sparkles: Added Other component and Subscribe Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
-import Trending from 'app/(home)/Trending'
-import Tech from './(home)/Tech'
-import Travel from './(home)/Travel'
-
+import Trending from "app/(home)/Trending";
+import Tech from "./(home)/Tech";
+import Travel from "./(home)/Travel";
+import Other from "app/(shared)/Other"
+import Subscribe from "./(shared)/Subscribe";
 
 export default function Home() {
   return (
-    <main className='px-10 leading-7'>
+    <main className="px-10 leading-7">
       <Trending />
-      <div className='md:flex gap-10 mb-5'>
-        <div className='basis-3/4'>
+      <div className="md:flex gap-10 mb-5">
+        <div className="basis-3/4">
           <Tech />
           <Travel />
-          {/*
-          <Other /> */}
-          <div className='hidden md:block'>
-          {/* <Subscribe /> */}
-          </div>
+          <Other />
+          <div className="hidden md:block"><Subscribe /></div>
         </div>
-        <div className='basis-1/4'>
-          {/* <Sidebar /> */}
-        </div>
+        <div className="basis-1/4">{/* <Sidebar /> */}</div>
       </div>
     </main>
-  )
+  );
 }
